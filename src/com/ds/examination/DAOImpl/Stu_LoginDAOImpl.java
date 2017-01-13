1b0ab85877b704992bb9d10cb0f4353421b72d6e
17727695227 <13124937709@163.com>
2017-01-13 17:39:47 +0800

    zhiweidalao

--- src/com/ds/examination/DAOImpl/Stu_LoginDAOImpl.java
+++ src/com/ds/examination/DAOImpl/Stu_LoginDAOImpl.java
@@ -1,7 +1,10 @@
 package com.ds.examination.DAOImpl;
 
+import org.springframework.stereotype.Repository;
+
 import com.ds.examination.DAO.Stu_LoginDAO;
 
+@Repository
 public class Stu_LoginDAOImpl implements Stu_LoginDAO {
 
 	public boolean isEmty(String username) throws Exception {
--- src/com/ds/examination/action/Stu_LoginAction.java
+++ src/com/ds/examination/action/Stu_LoginAction.java
@@ -1,12 +1,10 @@
 package com.ds.examination.action;
+import javax.annotation.Resource;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Scope;
 import org.apache.struts2.convention.annotation.Result;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.Scope;
 
 import com.ds.examination.service.Stu_LoginService;
 import com.opensymphony.xwork2.ActionSupport;
@@ -30,24 +28,17 @@ public class Stu_LoginAction extends ActionSupport {
 		this.username = username;
 	}
 
-
-	public Stu_LoginAction() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-		stu_LoginService = (Stu_LoginService)ctx.getBean("stu_LoginService");
-	}
-	
 	
 	public Stu_LoginService getStu_LoginService() {
 		return stu_LoginService;
 	}
 
-
+	@Resource
 	public void setStu_LoginService(Stu_LoginService stu_LoginService) {
 		this.stu_LoginService = stu_LoginService;
 	}
 
 
-	@Override
 	public String execute() throws Exception {
 		
 		System.out.println(stu_LoginService.isEmty(username));
